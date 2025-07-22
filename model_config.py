6b9343945d9a4081d2d4ccb2564576770edbfd77
Shanquan Tian <sqsqtian@gmail.com>
2025-07-21 22:51:29 -0400

    Added the small sizes

--- model_config.py
+++ model_config.py
@@ -16,19 +16,18 @@ class ModelConfig:
     use_bias (bool): Whether to use bias in Linear and LayerNorm layers. Default is True. Setting this to False can make the model a bit better and faster.
   """
   # The token embeddings. For example, we have vocab_size words, each word is a vector of size dim_embedding.
-  # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency.
-  vocab_size: int = 50304
+  # GPT-2 vocab_size is 50257, padded up to nearest multiple of 64 for efficiency.]
+  # In GPT_BY_HAND, we use 64 as the vocabulary size.
+  vocab_size: int = 64  # 50304
   # The position embeddings. For example, in a sentence we have block_size / seq_length positions,
   # each position is a vector of size dim_embedding.
   # It tells us the position of the word in the sentence.
-  seq_length: int = 1024
-  num_layers: int = 12
-  num_heads: int = 12
+  seq_length: int = 16  # GPT2 uses 1024
+  num_layers: int = 2  # GPT2 uses 12
+  num_heads: int = 2  # GPT2 uses 12
   # The embedding dimensionality.
-  dim_embedding: int = 768
+  dim_embedding: int = 32  # GPT2 uses 768
   dropout_rate: float = 0.0
-  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+  # True: bias in Linears and LayerNorms, like GPT-2. 
+  # False: a bit better and faster
   use_bias: bool = False
-  # The dtype to be used in the JAX model `jax_model.py`.
-  # Please note that it is not used in PyTorch `model.py`.
-  dtype: jnp.dtype = jnp.float32
